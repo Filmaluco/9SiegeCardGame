0ac635bf51dfc4d541f8ed6362474efbcc604b5b
FilipeA <filipera1995@hotmail.com>
2018-04-09 17:33:00 +0100

    Added: Battlefield Concept

--- 9SiegeCardGame/src/Controllers/GameController.java
+++ 9SiegeCardGame/src/Controllers/GameController.java
@@ -1,6 +1,13 @@
 package Controllers;
 
+import Models.EnemyTrackerModel;
+
 public class GameController {
+
+    EnemyTrackerModel battlefield = new EnemyTrackerModel();
+    // protected Map<Integer, CardModel> Deck;
+    // cant use shuffle, maybe create DeckModel?
+
 
 
 
