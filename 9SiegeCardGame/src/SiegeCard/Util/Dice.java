994e07d0d3a785616ddb2d8e2157b8a157068982
FilipeA <filipera1995@hotmail.com>
2018-04-10 16:21:29 +0100

    Updated: Initial DiceLogic Implemented
    Todo: complete for every case

--- 9SiegeCardGame/src/SiegeCard/Util/Dice.java
+++ 9SiegeCardGame/src/SiegeCard/Util/Dice.java
@@ -1,23 +1,54 @@
 package SiegeCard.Util;
 
+import java.util.Random;
+
 public class Dice {
 
-
-
-    private boolean morale = false,
+    private boolean close_combat_attack = false,
+                    normal_attack = false,
+                    circle_attack = false,
+                    battering_ram = false,
+                    ladder = false,
+                    siege_tower = false,
                     supplies = false,
-                    sabotage= false,
+                    sabotage = false,
                     raid = false,
-                    circle = false,
-                    batteringRam = false,
-                    coupure = false,
-                    closeCombate = false,
-                    siegeTower = false,
-                    ladder = false;
+                    rally = false,
+                    coupure = false;
 
+    private int modifier;
 
-    public Dice(String target, int modifier){
-        target = target.toUpperCase();
+    public Dice(int target_rule, int modifier){
 
+        this.modifier = modifier;
+        //The next piece of code is a sample:
+        //one target can change only a flag
+        if(target_rule == rule.REPAIRED_TREBUCHET){ coupure = true;}
+        // or multiple
+        if(target_rule == rule.COVER_OF_DARKNESS){ raid = true; sabotage = true;}
+        //....
+
+        //TODO: comple for all rules
     }
+
+    public int roll(int target_roll){
+        Random r = new Random();
+        int roll = r.nextInt(6);
+
+        //TODO: Apply same logic to all others
+        if(target_roll == roll_for.COUPURE_ROLL && coupure) {roll += modifier;}
+
+
+
+        roll = roll > 6 ? 6 : roll;
+        roll = roll < 0 ? 0 : roll;
+        return roll;
+    }
+
+    public int roll(){
+        Random r = new Random();
+        return r.nextInt(6);
+    }
+
+
 }
--- 9SiegeCardGame/src/SiegeCard/Util/roll_for.java
+++ 9SiegeCardGame/src/SiegeCard/Util/roll_for.java
@@ -1,20 +1,20 @@
 package SiegeCard.Util;
 
-public class values {
+public class roll_for {
 
-    public static int CLOSE_COMBAT_ATTACK           = 0;
     public static int NORMAL_ATTACK_BATTERING_RAM   = 10;
     public static int NORMAL_ATTACK_LADDER          = 11;
     public static int NORMAL_ATTACK_SIEGETOWER      = 12;
-    public static int BOILING_ATTACK_BATTERING_RAM  = 21;
-    public static int BOILING_ATTACK_LADDER         = 22;
-    public static int BOILING_ATTACK_SIEGETOWER     = 23;
+    public static int CIRCLE_ATTACK_BATTERING_RAM   = 10;
+    public static int CIRCLE_ATTACK_LADDER          = 11;
+    public static int CIRCLE_ATTACK_SIEGETOWER      = 12;
+    public static int BOILING_ATTACK_BATTERING_RAM  = 31;
+    public static int BOILING_ATTACK_LADDER         = 32;
+    public static int BOILING_ATTACK_SIEGETOWER     = 33;
     public static int ENEMY_LINE_CHECK              = 1;
     public static int SUPPLY_RAID_ROLL              = 2;
     public static int SABOTAGE_ROLL                 = 3;
     public static int COUPURE_ROLL                  = 4;
     public static int RALLY_TROOPS                  = 5;
-
-
-
 }
+
