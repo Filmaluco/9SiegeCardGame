14511bc5c22add59f69a0a3ab33337888eb1a93d
FilipeA <filipera1995@hotmail.com>
2018-04-10 15:36:31 +0100

    Added: "Global" variables
    Dice control variables

--- 9SiegeCardGame/src/SiegeCard/Util/values.java
+++ 9SiegeCardGame/src/SiegeCard/Util/values.java
@@ -2,7 +2,18 @@ package SiegeCard.Util;
 
 public class values {
 
-    public static int ATTACK_BATTERING_RAM = 0;
+    public static int CLOSE_COMBAT_ATTACK           = 0;
+    public static int NORMAL_ATTACK_BATTERING_RAM   = 10;
+    public static int NORMAL_ATTACK_LADDER          = 11;
+    public static int NORMAL_ATTACK_SIEGETOWER      = 12;
+    public static int BOILING_ATTACK_BATTERING_RAM  = 21;
+    public static int BOILING_ATTACK_LADDER         = 22;
+    public static int BOILING_ATTACK_SIEGETOWER     = 23;
+    public static int ENEMY_LINE_CHECK              = 1;
+    public static int SUPPLY_RAID_ROLL              = 2;
+    public static int SABOTAGE_ROLL                 = 3;
+    public static int COUPURE_ROLL                  = 4;
+    public static int RALLY_TROOPS                  = 5;
 
 
 
