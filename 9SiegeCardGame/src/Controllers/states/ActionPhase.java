9d3ad5cfb7b9c862dc97b32d0ce2ba97ebbced4c
FilipeA <filipera1995@hotmail.com>
2018-05-13 22:31:21 +0100

    Added: CloseCombat

--- 9SiegeCardGame/src/Controllers/states/ActionPhase.java
+++ 9SiegeCardGame/src/Controllers/states/ActionPhase.java
@@ -2,6 +2,8 @@ package Controllers.states;
 
 import Models.GameDataModel;
 
+import static SiegeCard.Util.rolls.CLOSE_COMBAT;
+
 public class ActionPhase extends StateAdapter {
 
     public ActionPhase(GameDataModel g) { super(g); }
@@ -37,6 +39,25 @@ public class ActionPhase extends StateAdapter {
     @Override
     public IState TrackSelectionBoilingAttack(){
         return getGame().canBoilAttack() ? new ActionBoilingAttack(getGame()): this;
+    }
+
+    @Override
+    public IState ActionCloseCombat(){
+        getGame().Player.removeActionPoint();
+        if(getGame().EnemyTracker.batteringRam.onCloseCombat()) { //Target is on Close Combat
+            if (getGame().Dice.roll(CLOSE_COMBAT) > getGame().EnemyTracker.batteringRam.getStrength())
+                getGame().EnemyTracker.batteringRam.retreat();
+        }else if(getGame().EnemyTracker.ladder.onCloseCombat()) { //Target is on Close Combat
+            if (getGame().Dice.roll(CLOSE_COMBAT) > getGame().EnemyTracker.ladder.getStrength())
+                getGame().EnemyTracker.ladder.retreat();
+        }else{
+            if (getGame().Dice.roll(CLOSE_COMBAT) > getGame().EnemyTracker.siegeTower.getStrength())
+                getGame().EnemyTracker.siegeTower.retreat();
+        }
+
+        if(getGame().Dice.getLastRoll() == 1) getGame().Player.tracker.reduceMorale();
+
+        return this;
     }
 
     @Override
