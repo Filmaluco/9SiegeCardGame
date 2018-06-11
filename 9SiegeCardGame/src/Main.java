ea64b2fc0c2b50d87ec27c29edc2b906204e034e
FilipeA <filipera1995@hotmail.com>
2018-06-11 21:30:52 +0100

    Added: TunnelView Assets

--- 9SiegeCardGame/src/Main.java
+++ 9SiegeCardGame/src/Main.java
@@ -1,11 +1,12 @@
 import Controllers.GameController;
+import Controllers.ObservableGame;
+import UI.Graphical.SiegeCardGUI;
 import UI.Text.SiegeCardUI;
 
 public class Main {
 
     public static void main(String[] args) {
-        GameController game = new GameController();
-        SiegeCardUI text = new SiegeCardUI(game);
-        text.start();
+        ObservableGame game = new ObservableGame();
+        new SiegeCardGUI(game);
     }
 }
