5bdc4bccf3239ccc8336829084f4a1fc6b5a8cf0
Ricardo <ricardoslyox@gmail.com>
2018-06-18 03:36:36 +0100

    Updated: TextUI Main

--- 9SiegeCardGame/src/Main.java
+++ 9SiegeCardGame/src/Main.java
@@ -6,10 +6,8 @@ import UI.Text.SiegeCardUI;
 public class Main {
 
     public static void main(String[] args) {
-        //GameController game = new GameController();
-        //SiegeCardUI text = new SiegeCardUI(game);
-        //text.start();
-        ObservableGame game = new ObservableGame();
-        new SiegeCardGUI(game);
+        GameController game = new GameController();
+        SiegeCardUI text = new SiegeCardUI(game);
+        text.start();
     }
 }
